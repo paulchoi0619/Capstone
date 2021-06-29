465f110cb0b70670ec23829ce671c6754d013300
polly0619 <78048114+polly0619@users.noreply.github.com>
2021-06-30 05:24:58 +0900

    rudimentary design+added datastructure for storing files

--- javascript/80s.js
+++ javascript/80s.js
@@ -13,16 +13,31 @@ let answer;
 let audio;
 let source;
 let randomOption;
-let randomSelect;
 let artist;
-let songs = [{fileName:"living.mp3", song:"Living on a Prayer", artist: "Bon Jovi",},
+
+let songs = [{fileName:"living.mp3", song:"Living on a Prayer", artist: "Bon Jovi"},
 {fileName: "take.mp3", song:"Take on Me", artist: "Aha"},
 {fileName:"wake.mp3", song:"Wake Me Up Before You Go-Go", artist:"Wham!"},
-
+{fileName:"called.mp3", song:"Crazy Little Thing Called Love", artist:"Queen"},
+{fileName:"sweet.mp3", song:"Sweet Dreams", artist:"Eurythmics"}
 ];
 let btn=[];
 let start = document.querySelector(".start");
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  while (0 !== currentIndex) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
 
 function updateScore(){
   ctx.fillStyle = 'white';
@@ -41,10 +56,12 @@ start.addEventListener('click', function(event){
    
     //updateScore();
  event.preventDefault();
- randomNumber= Math.floor(Math.random() * (songs.length-1));
+ randomNumber= Math.floor(Math.random() * (songs.length));
+ console.log(randomNumber);
  object.file = songs[randomNumber].fileName;
 object.artist= songs[randomNumber].artist;
 object.answer = songs[randomNumber].song;
+
 songs.splice(randomNumber,1);
 audio = document.createElement("audio");
  audio.id = "audio";
@@ -54,10 +71,11 @@ audio = document.createElement("audio");
 
 source = document.createElement("source");
 source.id="source";
-source.src="object.file";
+source.src="";
+
 source.type="audio/mpeg";
 
-source.setAttribute("src", file);
+source.setAttribute("src", "/Users/Polly/Documents/GitHub/Capstone/javascript/"+object.file)
 audio.appendChild(source);
 document.body.appendChild(audio);
 
@@ -65,19 +83,25 @@ document.body.appendChild(audio);
 let divBtn = document.createElement("div");
 divBtn.id = "container";
 document.body.appendChild(divBtn);
-randomOption = Math.floor(Math.random() * 3);
-randomSelect = Math.floor(Math.random() * songs.length-1);
+randomOption = Math.floor(Math.random() * 4);
+
+shuffle(songs);
+
+
 for(let i =0;i<4;i++){
 
-let btnName = "button"+i;
+let btnName = "btn"+i;
 btn[i] = document.createElement("button");
 if(i==randomOption){
+ console.log(randomOption)
 btn[i].innerText = object.answer;
-console.log(object.answer);
+
 }
 else{
-btn[i].innerText = songs[randomSelect].song;
+
+btn[i].innerText = songs[i].song;
 }
+
 btn[i].type = "button";
 btn[i].className=btnName;
 
