00c1b42eaa1c22efb27897b10b99283182fa9e5e
polly0619 <78048114+polly0619@users.noreply.github.com>
2021-06-30 14:18:33 +0900

    Added interface for game+option buttons+design+timer

--- javascript/80s.js
+++ javascript/80s.js
@@ -4,6 +4,11 @@ let ctx = score.getContext("2d");
 let totalScore =0;
 let question =0;
 */
+let totalscore=0;
+let current=1;
+let timeTick
+let timeOver;
+let question=5;
 let clicked = false;
 let object = {};
 let randomNumber;
@@ -14,12 +19,12 @@ let audio;
 let source;
 let randomOption;
 let artist;
-
-let songs = [{fileName:"living.mp3", song:"Living on a Prayer", artist: "Bon Jovi"},
-{fileName: "take.mp3", song:"Take on Me", artist: "Aha"},
-{fileName:"wake.mp3", song:"Wake Me Up Before You Go-Go", artist:"Wham!"},
-{fileName:"called.mp3", song:"Crazy Little Thing Called Love", artist:"Queen"},
-{fileName:"sweet.mp3", song:"Sweet Dreams", artist:"Eurythmics"}
+let btnName;
+let songs = [{fileName:"living.mp3", song:"Living on a Prayer", artist: "Bon Jovi",pic:"bon.jpg"},
+{fileName: "take.mp3", song:"Take on Me", artist: "Aha", pic:"aha.jpg"},
+{fileName:"wake.mp3", song:"Wake Me Up Before You Go-Go", artist:"Wham!", pic:"wham.jpg"},
+{fileName:"called.mp3", song:"Crazy Little Thing Called Love", artist:"Queen", pic:"queen.jpg"},
+{fileName:"sweet.mp3", song:"Sweet Dreams", artist:"Eurythmics", pic:"eurythmics.jpg"}
 ];
 let btn=[];
 let start = document.querySelector(".start");
@@ -46,7 +51,61 @@ function updateScore(){
   ctx.font= "60px Monaco";
   ctx.fillText(totalScore,130,100);
   }
-  
+function optionGenerator(){
+  randomOption = Math.floor(Math.random() * 4);
+  shuffle(songs);
+  for(let i =0;i<4;i++){
+
+    
+    if(i==randomOption){
+     console.log(randomOption)
+    btn[i].innerText = object.answer;
+    
+    }
+    else{
+    
+    btn[i].innerText = songs[i].song;
+    }
+    
+}
+}
+//reset function
+function reset(){
+  randomNumber= Math.floor(Math.random() * (songs.length));
+  object.file = songs[randomNumber].fileName;
+ object.artist= songs[randomNumber].artist;
+ object.answer = songs[randomNumber].song;
+
+ source.setAttribute("src", "/Users/Polly/Documents/GitHub/Capstone/javascript/"+object.file);
+ audio.load();
+ optionGenerator();
+}
+function setTime(){
+  if(current==question){
+    clearInterval(timeTick);
+    clearInterval(timerOver);
+  }
+  else{
+  current++;
+
+  reset();
+  }
+}
+let time =10;
+let timeElement =document.querySelector(".timer");
+
+function timer(){
+ 
+  time--;
+  setTimeout(function(){ timeElement.innerHTML = time;}, 900);
+  if(time==0){
+    clearInterval(timeTick);
+    time=10;
+    timeElement.innerHTML = time;
+    timeTick = setInterval(timer, 1000);
+  }
+}
+
 start.addEventListener('click', function(event){
 
   if (clicked == false){
@@ -56,8 +115,9 @@ start.addEventListener('click', function(event){
    
     //updateScore();
  event.preventDefault();
+
  randomNumber= Math.floor(Math.random() * (songs.length));
- console.log(randomNumber);
+
  object.file = songs[randomNumber].fileName;
 object.artist= songs[randomNumber].artist;
 object.answer = songs[randomNumber].song;
@@ -80,6 +140,7 @@ audio.appendChild(source);
 document.body.appendChild(audio);
 
 
+
 let divBtn = document.createElement("div");
 divBtn.id = "container";
 document.body.appendChild(divBtn);
@@ -90,7 +151,7 @@ shuffle(songs);
 
 for(let i =0;i<4;i++){
 
-let btnName = "btn"+i;
+btnName = "btn"+i;
 btn[i] = document.createElement("button");
 if(i==randomOption){
  console.log(randomOption)
@@ -109,170 +170,69 @@ divBtn.appendChild(btn[i]);
 }
 
 
-/*
+
 
 btn[0].addEventListener("click", function() {
-  if(combo[randomNumber].includes("polish")){
-    totalScore++;
-    updateScore();
-    if(question ==10){
-      alert("Correct! Total Score is "+totalScore);
-      location.reload();
-    }
-    else{
+  clearInterval(timeOver);
+  if(btn[0].innerText == object.answer){
     alert("Correct");
-   
-    combo.splice(randomNumber,1);
-    randomNumber= Math.floor(Math.random() * (combo.length-1));
-    song= combo[randomNumber];
-     
-    source.setAttribute("src", song+".mp4");
-    video.load();
-    question++;
-    }
-    
   }
   else{
-    if(question ==10){
-      alert("Wrong! Total Score is "+totalScore);
-      location.reload();
-    }
-    else{
-    alert("Wrong");
-    combo.splice(randomNumber,1);
-    randomNumber= Math.floor(Math.random() * (combo.length-1));
-    song= combo[randomNumber];
-      
-      source.setAttribute("src", song+".mp4");
-      video.load();
-      question++;
-    }
+    alert("Incorrect");
   }
-
+  reset();
 });
 
 
 
 
 btn[1].addEventListener("click", function() {
-  if(combo[randomNumber].includes("korean")){
-    totalScore++;
-    updateScore();
-    if(question ==10){
-      alert("Correct! Total Score is "+totalScore);
-      location.reload();
-    }
-    else{
+  clearInterval(timeOver);
+  if(btn[1].innerText == object.answer){
     alert("Correct");
-    combo.splice(randomNumber,1);
-    randomNumber= Math.floor(Math.random() * (combo.length-1));
-    song= combo[randomNumber];
-      
-      source.setAttribute("src", song+".mp4");
-      video.load();
-      question++;
-    }
   }
   else{
-    if(question ==10){
-      alert("Wrong! Total Score is "+totalScore);
-      location.reload();
-    }
-    else{
-    alert("Wrong");
-    combo.splice(randomNumber,1);
-    randomNumber= Math.floor(Math.random() * (combo.length-1));
-    song= combo[randomNumber];
-    
-      source.setAttribute("src", song+".mp4");
-      video.load();
-      question++;
-    }
+    alert("Incorrect");
   }
-  
-});
+  reset();
 
 
-
-btn[2].addEventListener("click", function() {
-  if(combo[randomNumber].includes("georgian")){
-    totalScore++;
-    updateScore();
-    if(question ==10){
-      alert("Correct! Total Score is "+totalScore);
-      location.reload();
-    }
-    else{
-    alert("Correct");
-    combo.splice(randomNumber,1);
-    randomNumber= Math.floor(Math.random() * (combo.length-1));
-    song= combo[randomNumber];
-     
-      source.setAttribute("src", song+".mp4");
-      video.load();
-      question++;}
-  }
-  else{
-    if(question ==10){
-      alert("Wrong! Total Score is "+totalScore);
-      location.reload();
-    }
-    else{
-    alert("Wrong");
-    combo.splice(randomNumber,1);
-      randomNumber= Math.floor(Math.random() * (combo.length-1));
-      song= combo[randomNumber];
-     
-      source.setAttribute("src", song+".mp4");
-      video.load();
-      question++;
-    }
-  }
-
-    
  
 });
 
 
 
-btn[3].addEventListener("click", function() {
-  if(combo[randomNumber].includes("spanish")){
-    totalScore++;
-    updateScore();
-    if(question ==10){
-      alert("Correct! Total Score is "+totalScore);
-      location.reload();
-    }
-    else{
+btn[2].addEventListener("click", function() {
+  clearInterval(timeOver);
+  if(btn[2].innerText == object.answer){
     alert("Correct");
-    combo.splice(randomNumber,1);
-    randomNumber= Math.floor(Math.random() * (combo.length-1));
-    song= combo[randomNumber];
-    
-      source.setAttribute("src", song+".mp4");
-      video.load();
-      question++;
-    }
   }
   else{
-    if(question ==10){
-      alert("Wrong! Total Score is "+totalScore);
-      location.reload();
-    }
-    else{
-    alert("Wrong");
-    combo.splice(randomNumber,1);
-    randomNumber= Math.floor(Math.random() * (combo.length-1));
-    song= combo[randomNumber];
-      
-      source.setAttribute("src", song+".mp4");
-      video.load();
-      question++;
-    }
+    alert("Incorrect");
   }
   
+  reset();
+ 
+
 });
-*/
+
+
+
+btn[3].addEventListener("click", function() {
+  clearInterval(timeOver);
+  if(btn[3].innerText == object.answer){
+    alert("Correct");
+  }
+  else{
+    alert("Incorrect");
+  }
+  
+  reset();
+
+});
+timeOver = setInterval(setTime, 10000);
+timeTick = setInterval(timer, 1000);
+
 clicked = true;
 }
 
